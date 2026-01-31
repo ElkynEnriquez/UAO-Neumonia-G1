7378130bb0d78d4e85b7c2dfe456143867dc8de9
maomauro <maomauro.c@gmail.com>
2026-01-31 14:40:38 -0500

    feature: se crea la carpeta models

---
+++ src/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Módulo de modelos.
+Contiene la lógica para cargar y gestionar modelos de machine learning.
+"""
+
+from .load_model import load_model, model_fun
+
+__all__ = ['load_model', 'model_fun']
